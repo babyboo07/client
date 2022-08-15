948e3c8316c1a1e0dbcc62e4e3c6f45cfee0a0d3
babyboo07 <emma28072002@gmail.com>
2022-08-15 17:40:45 +0700

    update

--- Connect.js
+++ Connect.js
@@ -2,28 +2,29 @@ const { Mail } = require("@mui/icons-material");
 const mongoose = require("mongoose");
 const UserModel = require("./src/Backend/Model/UserModel.js");
 
-mongoose.connect(
-  "mongodb+srv://maipn:PDhIC1KVODATQ8B9@cluster0.xtarnvd.mongodb.net/sharedVideo?retryWrites=true&w=majority"
-);
+mongoose.connect(process.env.MONGODB_URL, (error) => {
+  if (error) console.log(error);
+  else console.log("Connected successfully");
+});
 
 var db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'connection error:'));
+db.on("error", console.error.bind(console, "connection error:"));
 
-
-db.once('open', function(){
+db.once("open", function () {
   console.log("Connect successfully");
 
-  const user = new UserModel({name:"hehehe" , email:"hehe@gmail.com"})
+  const user = new UserModel({ name: "hehehe", email: "hehe@gmail.com" });
 
-  user.save(function(err,user){
-    if(err){console.log(err);}
-    console.log(user.name+ "hhhhhhh");
-  })
-})
+  user.save(function (err, user) {
+    if (err) {
+      console.log(err);
+    }
+    console.log(user.name + "hhhhhhh");
+  });
+});
 async function run() {
   try {
-    
   } catch (e) {
     console.log(e);
   }
